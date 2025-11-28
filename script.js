d3d5cedcede2c4a4d3b896db48a1c5d074e59665
Gabriel Milanez <159274083+gmilanezz@users.noreply.github.com>
2025-11-28 11:24:29 -0300

    delete
    -> tirei a animação da escrita na página servico_web.html

--- script.js
+++ script.js
@@ -8,25 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   faqItems.forEach(item => {
     item.addEventListener("click", () => toggleFAQ(item));
   });
-
-  const elements = document.querySelectorAll(".typewriter");
-
-  elements.forEach((el, index) => {
-    const text = el.textContent.trim();
-    el.textContent = ""; 
-    el.style.opacity = "1";
-
-    let i = 0;
-
-    function typeChar() {
-      if (i < text.length) {
-        el.textContent += text.charAt(i);
-        i++;
-        setTimeout(typeChar, 28);
-      }
-    }
-    setTimeout(typeChar, index * 850);
-  });
 });
 
 window.addEventListener("scroll", () => {
@@ -42,4 +23,3 @@ window.addEventListener("scroll", () => {
     navbar.classList.toggle("open");
   });
 });
-
